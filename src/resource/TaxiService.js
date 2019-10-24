0ed1e35e59db16d3a6a257ee47522c3040c00def
manoj-makkuboy <jesuismanoj@gmail.com>
2019-10-24 17:20:50 +0530

    Add _distanceBetweenPoints function implementation

--- src/resource/TaxiService.js
+++ src/resource/TaxiService.js
@@ -7,6 +7,15 @@ class TaxiService {
         return this.cars
     }
 
+    _distanceBetweenPoints(coorindate1, coordinate2){
+        let a = coorindate1[0] - coordinate2[0];
+        let b = coorindate1[1] - coordinate2[1];
+
+        let distance = Math.sqrt( a*a + b*b );
+
+        return Math.round(distance)
+    }
+
 }
 
 module.exports = TaxiService
