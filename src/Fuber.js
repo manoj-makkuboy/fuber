80a210e019429d7687915b3a947a958e7b9918b4
manoj-makkuboy <jesuismanoj@gmail.com>
2019-10-25 00:15:43 +0530

    Initial Fuber setup

---
+++ src/Fuber.js
@@ -0,0 +1,11 @@
+let TaxiService = require("../src/resource/TaxiService")
+let Car = require("../src/resource/Car")
+
+let car1 = new Car({ currentLocation: { latitude: 25, longitude: 26 }, colour: "pink", averageKilometerPerHour: 15, isAvailable: false });
+let car2 = new Car({ currentLocation: { latitude: 30, longitude: 31 }, colour: "blue", averageKilometerPerHour: 15, isAvailable: true });
+let car3 = new Car({ currentLocation: { latitude: 30, longitude: 31 }, colour: "pink", averageKilometerPerHour: 15, isAvailable: true });
+let car4 = new Car({ currentLocation: { latitude: 25, longitude: 25 }, colour: "pink", averageKilometerPerHour: 15, isAvailable: true });
+
+let Fuber = new TaxiService([car1, car2, car3, car4])
+
+module.exports = Fuber
