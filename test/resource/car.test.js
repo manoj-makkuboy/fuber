5934e6e68ca70b4b5c89574d48d23a1882729a52
manoj-makkuboy <jesuismanoj@gmail.com>
2019-10-24 15:25:16 +0530

    Add semicolon to car test

--- test/resource/car.test.js
+++ test/resource/car.test.js
@@ -1,10 +1,10 @@
 const Car = require("../../src/resource/Car");
 
 test('Should create new car instance', () => {
-  let car1 = new Car({currentLocation: {latitude: 50, longitude: 60}, colour: "pink", averageKilometerPerHour: 15})
-  expect(car1.currentLocation.latitude).toBe(50)
-  expect(car2.currentLocation.longitude).toBe(60)
-  expect(car2.colour).toBe("pink")
-  expect(car2.averageKilometerPerHour).toBe(15)
+  let car1 = new Car({currentLocation: {latitude: 50, longitude: 60}, colour: "pink", averageKilometerPerHour: 15});
+  expect(car1.currentLocation.latitude).toBe(50);
+  expect(car1.currentLocation.longitude).toBe(60);
+  expect(car1.colour).toBe("pink");
+  expect(car1.averageKilometerPerHour).toBe(15);
 
 });
