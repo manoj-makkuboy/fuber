a4c4e37bbad7b8ae1f3d84e1826cfc2fb4549b3b
manoj-makkuboy <jesuismanoj@gmail.com>
2019-10-24 15:06:37 +0530

    Add test to create car instance

---
+++ test/resource/car.test.js
@@ -0,0 +1,10 @@
+const Car = require('../resource/Car');
+
+test('Should create new car instance', () => {
+  let car1 = new Car({currentLocation: {latitude: 50, longitude: 60}, colour: "pink", averageKilometerPerHour: 15})
+  expect(car1.currentLocation.latitude).toBe(50)
+  expect(car2.currentLocation.longitude).toBe(60)
+  expect(car2.colour).toBe("pink")
+  expect(car2.averageKilometerPerHour).toBe(15)
+
+});
