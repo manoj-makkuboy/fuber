10591ab638e51575472bd0ac7110cde42d0652b3
manoj-makkuboy <jesuismanoj@gmail.com>
2019-10-24 15:25:23 +0530

    Add constructor

--- src/resource/Car.js
+++ src/resource/Car.js
@@ -1,5 +1,10 @@
 class Car {
-    
+    constructor(args) {
+        this.currentLocation = args.currentLocation;
+        this.colour = args.colour;
+        this.averageKilometerPerHour = args.averageKilometerPerHour
+    }
+
 }
 
 module.exports = Car
