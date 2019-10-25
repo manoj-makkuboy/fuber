7a9f3dfabcb08b5a1df7c3816f799f09699e4af9
manoj-makkuboy <jesuismanoj@gmail.com>
2019-10-25 11:13:32 +0530

    Add ID for cars

--- src/Fuber.js
+++ src/Fuber.js
@@ -1,10 +1,10 @@
 let TaxiService = require("../src/resource/TaxiService")
 let Car = require("../src/resource/Car")
 
-let car1 = new Car({ currentLocation: { latitude: 25, longitude: 26 }, colour: "pink", averageKilometerPerHour: 15, isAvailable: false });
-let car2 = new Car({ currentLocation: { latitude: 30, longitude: 31 }, colour: "blue", averageKilometerPerHour: 15, isAvailable: true });
-let car3 = new Car({ currentLocation: { latitude: 30, longitude: 31 }, colour: "pink", averageKilometerPerHour: 15, isAvailable: true });
-let car4 = new Car({ currentLocation: { latitude: 25, longitude: 25 }, colour: "pink", averageKilometerPerHour: 15, isAvailable: true });
+let car1 = new Car({id:1, currentLocation: { latitude: 25, longitude: 26 }, colour: "pink", averageKilometerPerHour: 15, isAvailable: false });
+let car2 = new Car({id:2, currentLocation: { latitude: 30, longitude: 31 }, colour: "blue", averageKilometerPerHour: 15, isAvailable: true });
+let car3 = new Car({id:3, currentLocation: { latitude: 30, longitude: 31 }, colour: "pink", averageKilometerPerHour: 15, isAvailable: true });
+let car4 = new Car({id:4, currentLocation: { latitude: 25, longitude: 25 }, colour: "pink", averageKilometerPerHour: 15, isAvailable: true });
 
 let Fuber = new TaxiService([car1, car2, car3, car4])
 
--- src/resource/Car.js
+++ src/resource/Car.js
@@ -1,5 +1,6 @@
 class Car {
     constructor(args) {
+        this.id = args.id;
         this.currentLocation = args.currentLocation;
         this.colour = args.colour;
         this.averageKilometerPerHour = args.averageKilometerPerHour;
