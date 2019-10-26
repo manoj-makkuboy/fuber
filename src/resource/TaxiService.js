c290475506ef7dced6ac308c17b14ff150065e71
manoj-makkuboy <jesuismanoj@gmail.com>
2019-10-26 18:34:41 +0530

    Improve code readablility

--- src/resource/TaxiService.js
+++ src/resource/TaxiService.js
@@ -4,23 +4,23 @@ class TaxiService {
     }
 
     getNearestAvailableCar(customerLocation, cars = this.cars) {
-      let availableCars = cars.filter((car) => car.isAvailable === true ? true : false);
-      let distanceArray = availableCars.map((car) => this._distanceBetweenPoints([car.currentLocation.latitude, car.currentLocation.longitude], [customerLocation.latitude, customerLocation.longitude]));
-      let nearestDistanceIndex = distanceArray.indexOf(Math.min(...distanceArray))
+        let availableCars = cars.filter((car) => car.isAvailable);
+        let distanceArray = availableCars.map((car) => this._distanceBetweenPoints([car.currentLocation.latitude, car.currentLocation.longitude], [customerLocation.latitude, customerLocation.longitude]));
+        let nearestDistanceIndex = distanceArray.indexOf(Math.min(...distanceArray))
 
-      return availableCars[nearestDistanceIndex]
+        return availableCars[nearestDistanceIndex]
     }
 
     getNearestAvailableCarByColour(customerLocation, colour) {
-        let carsWithRequiredColors = this.cars.filter((car) => car.colour === colour ? true : false)
-        return this.getNearestAvailableCar(customerLocation, carsWithRequiredColors) 
+        let carsWithRequiredColors = this.cars.filter((car) => car.colour === colour)
+        return this.getNearestAvailableCar(customerLocation, carsWithRequiredColors)
     }
 
-    _distanceBetweenPoints(coorindate1, coordinate2){
+    _distanceBetweenPoints(coorindate1, coordinate2) {
         let a = coorindate1[0] - coordinate2[0];
         let b = coorindate1[1] - coordinate2[1];
 
-        let distance = Math.sqrt( a*a + b*b );
+        let distance = Math.sqrt(a * a + b * b);
 
         return Math.round(distance)
     }
