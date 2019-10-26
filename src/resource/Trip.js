92cf7d0f99491b560e397e69b6fa4aa56089d3a1
manoj-makkuboy <jesuismanoj@gmail.com>
2019-10-26 22:36:33 +0530

    Auto increment ID for every trip object creation

--- src/resource/Trip.js
+++ src/resource/Trip.js
@@ -1,9 +1,16 @@
 class Trip {
     constructor(car, pickupLocation) {
+        this.id = Trip.setAutoIncrementId()
         this.car = car;
         this.pickupLocation = pickupLocation;
     }
 
+    static noOfTripsCreated = 0
+
+    static setAutoIncrementId(){
+        Trip.noOfTripsCreated = Trip.noOfTripsCreated + 1
+        return Trip.noOfTripsCreated
+    }
 
 }
 
--- test/resource/trip.test.js
+++ test/resource/trip.test.js
@@ -1,6 +1,11 @@
 const Car = require("../../src/resource/Car");
 const Trip = require("../../src/resource/Trip")
 
+
+beforeEach(() => {
+    Trip.noOfTripsCreated = 0;
+});
+
 test('Should create new car instance', () => {
     let car1 = new Car({
         id: 1,
@@ -11,5 +16,26 @@ test('Should create new car instance', () => {
     let pickupLocation = { pickupLocation: { latitude: 30, longitude: 40 } }
     let trip1 = new Trip(car1, pickupLocation);
     expect(trip1.car).toBe(car1);
+
+});
+
+test('Should Auto Increment ID on create of new trip', () => {
+    let car1 = new Car({
+        id: 1,
+        currentLocation: { latitude: 50, longitude: 60 },
+        colour: "pink",
+        averageKilometerPerHour: 15
+    });
+    let pickupLocation = { pickupLocation: { latitude: 30, longitude: 40 } }
+
+    expect(Trip.noOfTripsCreated).toBe(0);
+
+    let trip1 = new Trip(car1, pickupLocation);
+    expect(Trip.noOfTripsCreated).toBe(1);
+    expect(trip1.id).toBe(1);
+
+    let trip2 = new Trip(car1, pickupLocation);
+    expect(Trip.noOfTripsCreated).toBe(2);
+    expect(trip2.id).toBe(2);
 
 });
