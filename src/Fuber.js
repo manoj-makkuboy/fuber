3a895cf581216097ff8eb1fd3d0c252abd362b41
manoj-makkuboy <jesuismanoj@gmail.com>
2019-11-01 14:25:56 +0530

    Add 2 cars on create of Fuber instance

--- src/Fuber.js
+++ src/Fuber.js
@@ -1,11 +1,9 @@
 let TaxiService = require("../src/resource/TaxiService")
 let Car = require("../src/resource/Car")
 
-let car1 = new Car({id:1, currentLocation: { latitude: 25, longitude: 26 }, colour: "pink", averageKilometerPerHour: 15, isAvailable: false });
-let car2 = new Car({id:2, currentLocation: { latitude: 30, longitude: 31 }, colour: "blue", averageKilometerPerHour: 15, isAvailable: true });
-let car3 = new Car({id:3, currentLocation: { latitude: 30, longitude: 31 }, colour: "pink", averageKilometerPerHour: 15, isAvailable: true });
-let car4 = new Car({id:4, currentLocation: { latitude: 25, longitude: 25 }, colour: "pink", averageKilometerPerHour: 15, isAvailable: true });
+let car1 = new Car({id:1, currentLocation: { latitude: 25, longitude: 26 }, colour: "pink", averageKilometerPerHour: 15 });
+let car2 = new Car({id:2, currentLocation: { latitude: 30, longitude: 31 }, colour: "blue", averageKilometerPerHour: 15 });
 
-let Fuber = new TaxiService([car1, car2, car3, car4])
+let Fuber = new TaxiService([car1, car2])
 
 module.exports = Fuber
